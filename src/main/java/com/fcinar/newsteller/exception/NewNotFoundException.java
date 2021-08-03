4b169830043259e4aca766c12f2693cb874d6613
Fatih Çınar <fcinar_38@hotmail.com>
2021-08-03 22:03:49 +0300

    Initial commit

---
+++ src/main/java/com/fcinar/newsteller/exception/NewNotFoundException.java
@@ -0,0 +1,7 @@
+package com.fcinar.newsteller.exception;
+
+public class NewNotFoundException extends RuntimeException {
+    public NewNotFoundException(String message) {
+        super(message);
+    }
+}
